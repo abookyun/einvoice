4a5d966ae0f955922c4c721d8dac54556038ff54
David Yun <abookyun@gmail.com>
2015-12-24 14:44:04 +0800

    Add Connection module

--- lib/einvoice.rb
+++ lib/einvoice.rb
@@ -1,5 +1,8 @@
 require "einvoice/version"
 require "einvoice/configuration"
+require "einvoice/connection"
+require "einvoice/api"
+require "einvoice/client"
 
 module Einvoice
   extend Configuration
---
+++ lib/einvoice/connection.rb
@@ -0,0 +1,25 @@
+require "faraday_middleware"
+
+module Einvoice
+  module Connection
+    private
+
+    def connection
+      options = {
+        headers: { "Accept" => "application/#{format}; charset=utf-8" },
+        url: endpoint
+      }
+
+      ::Faraday::Connection.new(options) do |connection|
+        connection.use Faraday::Request::UrlEncoded
+
+        case format.to_s.downcase
+        when "xml" then connection.use Faraday::Response::ParseXml
+        when "json" then connection.use Faraday::Response::ParseJson
+        end
+
+        connection.adapter Faraday.default_adapter
+      end
+    end
+  end
+end
