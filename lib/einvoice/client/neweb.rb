c762923873d804a599fa426d1bea3e37d53c26e1
David Yun <abookyun@gmail.com>
2016-01-11 15:39:17 +0800

    One purpose per methods and write some specs

--- lib/einvoice/client/neweb.rb
+++ lib/einvoice/client/neweb.rb
@@ -9,21 +9,29 @@ module Einvoice
           request.url endpoint + action
           request.body = {
             StoreCode: client_id,
-            xmldata: encode_xml(wrap(invoice))
+            xmldata: encode_xml(wrap(serialize(invoice)))
           }
         end.body
       end
 
       private
 
-      def encode_xml(data)
-        ::Gyoku.xml(data, key_converter: :none)
+      def serialize(invoice)
+        invoice.serializable_hash(except: [:errors, :validation_context], include: [:invoice_item, :contact, :customer_defined])
       end
 
-      def wrap(invoice)
+      def wrap(hash)
         { invoice_root:
-          { invoice: invoice.serializable_hash(except: [:errors, :validation_context], include: { invoice_item: {}, contact: {}, customer_defined: {}})}
-        }.deep_transform_keys { |k| k.to_s.camelize }
+          { invoice: hash }
+        }
+      end
+
+      def camelize(hash)
+        hash.deep_transform_keys { |k| k.to_s.camelize }
+      end
+
+      def encode_xml(hash)
+        ::Gyoku.xml(hash, key_converter: :none)
       end
     end
   end
