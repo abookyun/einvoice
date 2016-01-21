ac846dd8703e46ea45341f1e2e56e9d928a8f432
David Yun <abookyun@gmail.com>
2016-01-21 17:20:18 +0800

    Use VCR to record and spec outside API

--- lib/einvoice/neweb/result.rb
+++ lib/einvoice/neweb/result.rb
@@ -5,7 +5,7 @@ module Einvoice
         if response.is_a? ActiveModel::Errors
           response.full_messages.join('; ')
         else
-          data = response && (response.fetch("Result").fetch("Invoice", {}) || response.fetch("Result", {}))
+          data = response && (response["Result"]["Invoice"] || response["Result"])
           data["statcode"] == "0000" ? nil : "#{data["statcode"]}: #{data["statdesc"]}" if data
         end
       end
@@ -14,7 +14,7 @@ module Einvoice
         if response.is_a? ActiveModel::Errors
           false
         else
-          data = response && (response.fetch("Result").fetch("Invoice", {}) || response.fetch("Result", {}))
+          data = response && (response["Result"]["Invoice"] || response["Result"])
           data["statcode"] == "0000" if data
         end
       end
--- spec/einvoice/neweb/result_spec.rb
+++ spec/einvoice/neweb/result_spec.rb
@@ -9,21 +9,42 @@ RSpec.describe Einvoice::Neweb::Result do
   end
 
   describe "#errors" do
+    let(:client) { Einvoice::Client.new(Einvoice::Neweb::Provider.new) }
+
     it "has default value" do
       expect(subject.errors).to be_nil
     end
 
-    it "returns active model validation errors" do
-      invoice = build(:neweb_pre_invoice, data_number: nil)
-      invoice.valid?
+    context "with active model errors" do
+      it "returns active model validation errors" do
+        invoice = build(:neweb_pre_invoice, data_number: nil)
+        invoice.valid?
 
-      expect(described_class.new(invoice.errors).errors).to include "Data number can't be blank"
+        expect(described_class.new(invoice.errors).errors).to include "Data number can't be blank"
+      end
+    end
+
+    context "with API response errors" do
+      it "joins response errors as string", vcr: { record: :once } do
+        invoice = build(:neweb_pre_invoice, data_number: "53086054001")
+        sn = invoice.invoice_item.first.sequence_number.to_s
+        invoice_item = build(:neweb_invoice_item, sequence_number: sn)
+        invoice.invoice_item << invoice_item
+        expect(client.issue(invoice.payload).errors).to eq "7010: SequenceNumber不可重複"
+      end
     end
   end
 
   describe "#success?" do
+    let(:client) { Einvoice::Client.new(Einvoice::Neweb::Provider.new) }
+
     it "has default value" do
       expect(subject.errors).to be_falsey
+    end
+
+    it "returns true when there's no errors", vcr: { record: :once } do
+      invoice = build(:neweb_pre_invoice)
+      expect(client.issue(invoice.payload).success?).to be_truthy
     end
 
     it "returns false when there's active model errors" do
